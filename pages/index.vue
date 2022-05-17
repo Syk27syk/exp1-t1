70ff39db9987bfcab163e15367611987c0c98f7e
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-17 16:48:14 +0800

    start

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <h1 class="text-5xl"> Experiment 1 - try 1 </h1>
+      <p class="mt-5"> solid sphere floating on ocean </p>
+      <p class="mt-5"> threeJS experiment </p>
     </div>
   </div>
 </template>
